9881cd5d8912e21da9b42e866552fa4eb0045aa0
omuabormalin <omuabormalin@gmail.com>
2022-10-24 08:03:40 -0700

    i modified the code to verify if its user name or password that is wrong

--- conditional_statement/log_in.c
+++ conditional_statement/log_in.c
@@ -1,21 +1,32 @@
+/*this code is to validate user name and password already saved in the data base and grant access if data matches and if not the program ends after 3 wrong attempts.*/
+#include <stdio.h>
+/**
+ * main - hold the code for the entire proces
+ * @A: variable to accept user na,me
+ * @B: varioable to accept user password
+ * @c: variable holding the user name in the system already
+ * @d: variable holding the user password inm the system already
+ */
 int main()
 {
-	int a, A;
-	int b, B;
-	a = 1234;
-	b = 0;
-	int user;
+	int c, A;
+	int d, B;
+	c = 1234;
+	d = 1212;
 	   printf("Entre username: ");
 	      scanf("%d", &A);
 	       printf("Enter password: ");
 	          scanf("%d", &B);
-		   if
-			    (A == a && B == b)
-				     {
-					      printf("welcome %d", a);
-					       }
-		    else
+		   if(A == c && B == d)
+			    {
+				     printf("welcome %d", c);
+				      }
+		    else if(A != c)
 			     {
-				      printf("wrong username or password");
+				      printf("wrong username");
 				       }
-}
+		     else if(B != d)
+			      {
+				       printf("wrong password");
+				        }
+		      }
