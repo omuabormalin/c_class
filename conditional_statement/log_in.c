e25a7e6756f4abd280cda0dd7c05746de3b52e61
omuabormalin <omuabormalin@gmail.com>
2022-10-24 06:58:56 -0700

    created a file to validate user login

---
+++ conditional_statement/log_in.c
@@ -0,0 +1,21 @@
+int main()
+{
+	int a, A;
+	int b, B;
+	a = 1234;
+	b = 0;
+	int user;
+	   printf("Entre username: ");
+	      scanf("%d", &A);
+	       printf("Enter password: ");
+	          scanf("%d", &B);
+		   if
+			    (A == a && B == b)
+				     {
+					      printf("welcome %d", a);
+					       }
+		    else
+			     {
+				      printf("wrong username or password");
+				       }
+}
