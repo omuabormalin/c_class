5e2600e3a1014dd7c29a11974068b00e90fe2495
omuabor smart daniel <35344088+omuabormalin@users.noreply.github.com>
2022-10-26 19:31:18 +0100

    Update log_in.c

--- conditional_statement/log_in.c
+++ conditional_statement/log_in.c
@@ -1,111 +1,56 @@
 /*this code is to validate user name and password already saved in the data base and grant access if data matches and if not the program ends after 3 wrong attempts.*/
-
 #include <stdio.h>
-
 /**
- *
- *  * main - hold the code for the entire proces
- *
- *   * @A: variable to accept user na,me
- *
- *    * @B: varioable to accept user password
- *
- *     * @c: variable holding the user name in the system already
- *
- *      * @d: variable holding the user password inm the system already
- *
- *       */
-
+ * main - hold the code for the entire proces
+ * @A: variable to accept user na,me
+ * @B: varioable to accept user password
+ * @c: variable holding the user name in the system already
+ * @d: variable holding the user password inm the system already
+ */
  int login();
+ int login()
+ {
+     	int c, A;
+	int d, B;
+	c = 1234;
+	d = 1212;
+	int i = 0;
+	
 
-  int login()
-
-	 {
-
-		      	int c, A;
-
-				int d, B;
-
-					c = 1234;
-
-						d = 1212;
-
-							int i = 0;
-
-								
-
-
-
-								   printf("Entre username: ");
-
-								   	   scanf("%d", &A);
-
-									   	   printf("Enter password: ");
-
-										   	   scanf("%d", &B);
-
-											   	          
-
-											   			if (A ==c && B ==d)
-
-																		{
-
-																						printf("welcome");
-
-																								    }
-
-																    while(A != c || B != d)
-
-																	    		    {
-
-																				    		        printf("incorrect user name or pass word\n");
-
-																									     printf("Entre username: ");
-
-																									     	   scanf("%d", &A);
-
-																										   	   printf("Enter password: ");
-
-																											   	   scanf("%d", &B);
-
-																												   	          
-
-																												   			if (A ==c && B ==d)
-
-																																			{
-
-																																							printf("welcome");
-
-																																									    }
-
-																																	   	i++;
-
-																																				
-
-																																					if (i == 2 && A != c || B != d)
-
-																																									{
-
-																																													    printf("system locked");
-
-																																													    			}
-
-																																									if (i == 2)
-
-																																													{
-
-																																																	break;
-
-																																																				}
-
-																																									 }
-
-																     }
-
+	   printf("Entre username: ");
+	   scanf("%d", &A);
+	   printf("Enter password: ");
+	   scanf("%d", &B);
+	          
+			if (A ==c && B ==d)
+			{
+			printf("welcome");
+		    }
+		    while(A != c || B != d)
+		    {
+		        printf("incorrect user name or pass word\n");
+		     printf("Entre username: ");
+	   scanf("%d", &A);
+	   printf("Enter password: ");
+	   scanf("%d", &B);
+	          
+			if (A ==c && B ==d)
+			{
+			printf("welcome");
+		    }
+		   	i++;
+		
+			if (i == 2 && A != c || B != d)
+			{
+			    printf("system locked");
+			}
+				if (i == 2)
+			{
+			break;
+			}
+ }
+ }
 int main()
-
 {
-
-	login();
-
+login();
 }
